9a4270261ddaa7838eb033a1b8f4e1ad4321f764
sanchan09 <sandunisathsarani09@gmail.com>
2024-01-21 16:13:42 +0530

    Changed the firebase.js file and Register.jsx file to store the user's data in users database in firebase.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCkmO0H1ridZkav1Aleh18AYEEb4JoUoYE",
-  authDomain: "chat-89130.firebaseapp.com",
-  projectId: "chat-89130",
-  storageBucket: "chat-89130.appspot.com",
-  messagingSenderId: "801043327752",
-  appId: "1:801043327752:web:04cd2e431891f203771abd"
+  apiKey: "AIzaSyBS0cAarHmftPXt14RqMSjyBdq1LOokoUc",
+  authDomain: "chatapp-7e4f1.firebaseapp.com",
+  projectId: "chatapp-7e4f1",
+  storageBucket: "chatapp-7e4f1.appspot.com",
+  messagingSenderId: "88390215689",
+  appId: "1:88390215689:web:d92975efc03329f41bf0d8"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore()
