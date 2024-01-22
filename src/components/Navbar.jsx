c76968bb5abeac96164a801dd9ad423e4415536d
sanchan09 <sandunisathsarani09@gmail.com>
2024-01-22 13:45:43 +0530

    Modified three files named App.js, Navbar.jsx, index.js and added a new file named AuthContext.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../firebase'
 
 const Navbar = () => {
   return (
@@ -7,7 +9,7 @@ const Navbar = () => {
       <div className="user">
         <img src="https://images.pexels.com/photos/11440206/pexels-photo-11440206.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
         <span>Lazzy</span>
-        <button>logout</button>
+        <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
   )
